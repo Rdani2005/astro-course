b51935bc3693ea519a3261e181984a320938cee8
Rdani2005 <daniel.sequeira@betanysports.eu>
2026-02-03 11:38:23 -0600

    feat: adding author page

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,18 +1,34 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
-    // relation
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
+
+      // Relación
+      author: reference("author"),
+
+      // Relación
+      tags: z.array(z.string()),
+
+      isDraft: z.boolean().default(false),
+    }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
 });
 
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
